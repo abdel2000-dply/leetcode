d69739e983863602c035ef23b927e9c83de34bda
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-08-07 20:43:25 +0100

    Add solution for converting integer to English words

--- hard/273_integer_to_english_words.py
+++ hard/273_integer_to_english_words.py
@@ -1,4 +1,8 @@
+""" 
+273. Integer to English Words 
 
+Convert a non-negative integer num to its English words representation.
+"""
 class Solution:
     def numberToWords(self, num: int) -> str:
         if num == 0:
