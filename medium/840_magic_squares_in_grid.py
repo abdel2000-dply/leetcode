06f86d91883a6339868af26ad9df145afc674ca4
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-08-09 22:52:45 +0100

    Add solution for counting magic squares in a grid

--- medium/840_magic_squares_in_grid.py
+++ medium/840_magic_squares_in_grid.py
@@ -7,3 +7,33 @@ Given a row x col grid of integers, how many 3 x 3 contiguous magic square subgr
 
 Note: while a magic square can only contain numbers from 1 to 9, grid may contain numbers up to 15.
 """
+class Solution:
+    def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
+        magic_squares = [
+            [8, 1, 6, 3, 5, 7, 4, 9, 2],
+            [6, 1, 8, 7, 5, 3, 2, 9, 4],
+            [4, 9, 2, 3, 5, 7, 8, 1, 6],
+            [2, 9, 4, 7, 5, 3, 6, 1, 8],
+            [8, 3, 4, 1, 5, 9, 6, 7, 2],
+            [4, 3, 8, 9, 5, 1, 2, 7, 6],
+            [6, 7, 2, 1, 5, 9, 8, 3, 4],
+            [2, 7, 6, 9, 5, 1, 4, 3, 8]
+        ]
+        def is_magic(r: int, c: int) -> bool:
+            grid_v = [
+                grid[r][c], grid[r][c+1], grid[r][c+2],
+                grid[r+1][c], grid[r+1][c+1], grid[r+1][c+2],
+                grid[r+2][c], grid[r+2][c+1], grid[r+2][c+2]
+            ]
+            return grid_v in magic_squares
+
+        rows, cols = len(grid), len(grid[0])
+        count = 0
+
+        for i in range(rows - 2):
+            for j in range(cols - 2):
+                print(grid[i+1][j+1])
+                if grid[i+1][j+1] == 5:
+                    if is_magic(i,j):
+                        count += 1
+        return count
