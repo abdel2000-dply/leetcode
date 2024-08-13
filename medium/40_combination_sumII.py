29d29155932949396345821568b8f20430c67239
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-08-13 19:39:45 +0100

    Add solution for Combination Sum II problem

--- medium/40_combination_sumII.py
+++ medium/40_combination_sumII.py
@@ -1,3 +1,12 @@
 """
 40. Combination Sum II
+
+Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+
+Each number in candidates may only be used once in the combination.
+
+Note: The solution set must not contain duplicate combinations.
 """
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        
