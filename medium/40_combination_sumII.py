215ce8c57768942c6c2a72550c8e70e4719d29b7
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-08-13 19:40:00 +0100

    Add backtrack algorithm for Combination Sum II problem

--- medium/40_combination_sumII.py
+++ medium/40_combination_sumII.py
@@ -9,4 +9,21 @@ Note: The solution set must not contain duplicate combinations.
 """
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        
+        def backtrack(start, curr_comb, curr_sum):
+            if curr_sum == target:
+                result.append(curr_comb[:])
+                return
+            if curr_sum > target:
+                return
+
+            for i in range(start, len(candidates)):
+                if i > start and candidates[i] == candidates[i-1]:
+                    continue
+                curr_comb.append(candidates[i])
+                backtrack(i+1, curr_comb, curr_sum + candidates[i])
+                curr_comb.pop()
+
+        result = []
+        candidates.sort()
+        backtrack(0, [], 0)
+        return result
