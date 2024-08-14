0a9b8b395b5ddb9a42f61aea4127f1c5b5323944
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-08-14 21:23:49 +0100

    Implement binary search algorithm for finding K-th smallest pair distance

--- hard/719_find_K-th_smallest_pair_distance.py
+++ hard/719_find_K-th_smallest_pair_distance.py
@@ -5,5 +5,26 @@ The distance of a pair of integers a and b is defined as the absolute difference
 
 Given an integer array nums and an integer k, return the kth smallest distance among all the pairs nums[i] and nums[j] where 0 <= i < j < nums.length.
 
-
 """
+class Solution:
+    def smallestDistancePair(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        
+        def countPairs(mid):
+            count = 0
+            left = 0
+            for right in range(len(nums)):
+                while nums[right] - nums[left] > mid:
+                    left += 1
+                count += right - left
+            return count
+
+        low, high = 0, nums[-1] - nums[0]
+        while low < high:
+            mid = (low + high) // 2
+            if countPairs(mid) < k:
+                low = mid + 1
+            else:
+                high = mid
+                
+        return low
