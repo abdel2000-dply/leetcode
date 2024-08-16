4d1508e22eb90cd872b67ff439cc6d7137da5e60
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-08-16 18:09:16 +0100

    Add solution for 624. Maximum Distance in Arrays problem

--- medium/624_maximum_distance_in_arrays.py
+++ medium/624_maximum_distance_in_arrays.py
@@ -1,3 +1,17 @@
 '''624. Maximum Distance in Arrays
 
 '''
+class Solution:
+    def maxDistance(self, arrays: List[List[int]]) -> int:
+        min_v = arrays[0][0]
+        max_v = arrays[0][-1]
+        
+        max_dist = 0
+        
+        for i in range(1, len(arrays)):
+            max_dist = max(max_dist, abs(arrays[i][-1] - min_v), abs(max_v - arrays[i][0]))
+            
+            min_v = min(min_v, arrays[i][0])
+            max_v = max(max_v, arrays[i][-1])
+        
+        return max_dist
