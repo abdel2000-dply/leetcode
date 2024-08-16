f4c594fa4a3baaca8539aeadf7c38e384b7b2c63
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-08-16 18:09:47 +0100

    624. Maximum Distance in Arrays

--- medium/624_maximum_distance_in_arrays.py
+++ medium/624_maximum_distance_in_arrays.py
@@ -1,5 +1,10 @@
 '''624. Maximum Distance in Arrays
 
+You are given m arrays, where each array is sorted in ascending order.
+
+You can pick up two integers from two different arrays (each array picks one) and calculate the distance. We define the distance between two integers a and b to be their absolute difference |a - b|.
+
+Return the maximum distance.
 '''
 class Solution:
     def maxDistance(self, arrays: List[List[int]]) -> int:
