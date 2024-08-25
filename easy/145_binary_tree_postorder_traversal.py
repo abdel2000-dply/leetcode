22a410b4bcfb15243819fd7430e762006c7af0fd
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-08-25 21:49:19 +0100

    Add solution for 145. Binary Tree Postorder Traversal problem

--- easy/145_binary_tree_postorder_traversal.py
+++ easy/145_binary_tree_postorder_traversal.py
@@ -1,3 +1,18 @@
 """
 145. Binary Tree Postorder Traversal
+Given the root of a binary tree, return the postorder traversal of its nodes' values.
 """
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+
+        left_trav = self.postorderTraversal(root.left)
+        right_trav = self.postorderTraversal(root.right)
+        return left_trav + right_trav + [root.val]
