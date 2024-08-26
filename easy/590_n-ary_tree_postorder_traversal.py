f4a2ef549a300b68797be27008eb68f257b57a22
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-08-26 16:12:40 +0100

    Add solution for 590. N-ary Tree Postorder Traversal problem

---
+++ easy/590_n-ary_tree_postorder_traversal.py
@@ -0,0 +1,19 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        def helper(node):
+            if not node:
+                return
+            for child in node.children:
+                helper(child)
+            result.append(node.val)
+        result = []
+        helper(root)
+        return result
