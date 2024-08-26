c4a68a0c9574cb0ba0e672401444d7a18e76de05
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-08-26 16:13:44 +0100

    Implement N-ary Tree Postorder Traversal algorithm

--- easy/590_n-ary_tree_postorder_traversal.py
+++ easy/590_n-ary_tree_postorder_traversal.py
@@ -1,9 +1,10 @@
 """
 590. N-ary Tree Postorder Traversal
 
+Given the root of an n-ary tree, return the postorder traversal of its nodes' values.
 
-"""
-"""
+Nary-Tree input serialization is represented in their level order traversal. Each group of children is separated by the null value (See examples)
+
 # Definition for a Node.
 class Node:
     def __init__(self, val=None, children=None):
