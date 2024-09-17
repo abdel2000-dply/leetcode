4191d7e92921916f9f5841dd920a20e31156eff8
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-09-17 16:19:12 +0100

    solution of UncommonWordsfromTwoSentences

--- easy/UncommonWordsfromTwoSentences.py
+++ easy/UncommonWordsfromTwoSentences.py
@@ -8,3 +8,19 @@ A word is uncommon if it appears exactly once in one of the sentences, and does 
 Given two sentences s1 and s2, return a list of all the uncommon words. You may return the answer in any order.
 """
 
+class Solution:
+    def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
+        s1Words = s1.split(' ')
+        s2Words = s2.split(' ')
+
+        words = s1Words + s2Words
+        wordsCount = Counter(words)
+        uncommon = []
+        
+        for k, v in wordsCount.items():
+            if v == 1:
+                uncommon.append(k)
+
+        return uncommon
+
+        
